fb13c4f3cdbe5a59e53337c2985ab844c8551675
unknown <jtjackson325@gmail.com>
2022-10-18 15:42:34 -0600

    coded the main function for the Introvert component

---
+++ src/components/Introvert/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// holder for image tage of an introvert //
+
+function Introvert() {
+    retrun (
+        <section id="introvert" className="into">
+            <img  />
+            <div className="inverted">
+                <h2>Introverts</h2>
+                <p></p>
+            </div>
+        </section>
+    )
+}
+
+export default Introvert;
