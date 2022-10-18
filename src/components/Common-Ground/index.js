3f3d59a3792db50e5b80fc22e040c5875daa6354
unknown <jtjackson325@gmail.com>
2022-10-18 15:53:49 -0600

    got the function for the Common component coded

---
+++ src/components/Common-Ground/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// place holder for th image for this webpage //
+
+function Common() {
+    return (
+        <section id="shared" className="together">
+            <img />
+            <div className="peaceful">
+                <h2>Common Ground</h2>
+                <p></p>
+            </div>
+        </section>
+    )
+}
+
+export default Common;
