3f6a12a72ad668783c609ba1a4bba032b98121f0
unknown <jtjackson325@gmail.com>
2022-10-20 19:06:52 -0600

    got the setup for the commong ground page with the paragraph done

--- src/components/Common-Ground/index.js
+++ src/components/Common-Ground/index.js
@@ -4,11 +4,20 @@ import sharedThings from "../../assests/shared/common-ground.jpg"
 function Common() {
     return (
         <section id="shared" className="together">
-            <img src={sharedThings} alt="two people shaking hands" />
             <div className="peaceful">
                 <h2>Common Ground</h2>
-                <p></p>
+                <p>
+                   Things in Common: Introverts and extroverts both enjoy talking with other people 
+                   who are open and thrive on social interaction. Both personality types can get 
+                   anxious if they have to answer a question they were not prepared for. For example, 
+                   both personalities can find it nerve racking when a teacher randomly calls on them 
+                   to asnswer a question in class. Each personality might prefer to work alone when 
+                   they have to produce a quality product for their boss at work, teacher at school, etc. 
+                   For example, student’s tend to work by themselves whether they are introverted or 
+                   extroverted when a teacher makes an assignment worth the majority of their grade.  
+                </p>
             </div>
+            <img src={sharedThings} alt="two people shaking hands" />
         </section>
     )
 }
