0122141633b643033ad91df861bbdeb068a31787
unknown <jtjackson325@gmail.com>
2022-11-08 17:53:29 -0700

    got more information on introverts and extroverts

--- src/components/Extrovert/index.js
+++ src/components/Extrovert/index.js
@@ -7,21 +7,30 @@ function Extrovert() {
             <div className="going">
                 <h2>Extroverts</h2>
                 <p>
-                    General Definition: Extroverts in general are known for their confidence and 
-                    ability to talk to other people with ease. For example, extroverts are able to 
-                    start conversations with complete strangers without too much difficulty. The 
-                    next paragraph belows explains why extroverts are able to communicate with 
-                    others so easily.  
+                    In general extroverts are known for having lots of energy which usually makes them 
+                    outgoing and engage in social situations easily. Because of their energy they are 
+                    able to take initiative in situations without letting shyness or uneasiness get in 
+                    the way. The type of jobs extroverts excel in are jobs like being a sales person where 
+                    their ability to engage other people comes in handy. This page of the website is going 
+                    to talk about extroverts' endless energy and how they are viewed in a school setting. 
                 </p>
                 <br></br>
                 <p>
-                    Communicating with Others: Extroverts find communicating with others to be easy 
-                    because they like learning from other people. Learning from other people helps 
-                    extroverts understand the world around them better. The chance to learn new things 
-                    from others along with social interaction is what energizes extroverts.
+                    Extroverts are people who are outgoing in social situations and when they have a room full 
+                    of people they can talk to. Extroverts are outgoing in social situations because they like 
+                    hearing other people's perspective and gaining insight from it. Both learning from other 
+                    people and engaging in conversation energizes extroverted personalities and brings them joy 
+                    as they converse with other people. Extroverts start to lose energy when they are in situations 
+                    where the other people do not speak up or provide enough details to keep the conversation going. 
                 </p>
                 <p>
-                    This paragraph will also be on more additional information about extroverts. It will be worked on tomoroow
+                    In school is where extroverts are able to shine and demonstrate their ability to converse with 
+                    others easily. Because of their ability to easily converse with others they are the ones that 
+                    answer a teacher's question most often. Extroverts will be the students that are usually picked 
+                    for class discussions because they keep the teacher's lesson moving forward instead of having 
+                    the lesson drag on when no one else speaks up. Extroverts excel at group work since it gives them 
+                    a chance to collaborate with others but find solo work a little more challenging because they do 
+                    not have other people to re-energize them. 
                 </p>
             </div>
             <img src={extrovertDescription} alt="words that describe extroverts" />
