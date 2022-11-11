7744f90e9e28d6eb02041511e6627e7cfc15c310
unknown <jtjackson325@gmail.com>
2022-11-10 21:37:49 -0700

    added the additional paragraph on how communication between introverts and extroverted personalities can increase

--- src/components/Needed-Change/index.js
+++ src/components/Needed-Change/index.js
@@ -5,16 +5,32 @@ function Change() {
     return (
         <section id="shared" className="together">
             <div className="peaceful">
-                <h2>Common Ground</h2>
+                <h2>Needed Change</h2>
                 <p>
-                   Things in Common: Introverts and extroverts both enjoy talking with other people 
-                   who are open and thrive on social interaction. Both personality types can get 
-                   anxious if they have to answer a question they were not prepared for. For example, 
-                   both personalities can find it nerve racking when a teacher randomly calls on them 
-                   to asnswer a question in class. Each personality might prefer to work alone when 
-                   they have to produce a quality product for their boss at work, teacher at school, etc. 
-                   For example, student’s tend to work by themselves whether they are introverted or 
-                   extroverted when a teacher makes an assignment worth the majority of their grade.  
+                    One way to increase communication between introverted and extroverted students is 
+                    for introverted students to be more talkative when they engage in conversation. 
+                    College students who filled out my survey have expressed they enjoy talking to 
+                    extroverted people more because they love to keep the conversation going. Speaking 
+                    up will enable introverts to get along with extroverts easier because they are making 
+                    more of an effort to keep the conversation going. On the other hand, extroverted 
+                    personalities need to let introverted personalities recharge after talking to them 
+                    for a little while. For example, one college student I surveyed says he enjoys talking 
+                    to other people in social situations but finds it draining so he takes time to recharge 
+                    by himself when needed. Both of these things would increase communication between the 
+                    two because each personality is respecting each other's social preferences. 
+                </p>
+                <br></br>
+                <p>
+                    One thing college students have expressed that needs to happen more often in the classroom is 
+                    to have more activities where the classmates can work together in more small groups. Working 
+                    together in small groups can help introverts open up more and more as they get to be comfortable 
+                    conversing with their fellow classmates.  A way teachers can encourage conversation between 
+                    classmates is to facilitate discussions where students who do not usually speak up share their 
+                    opinions. Students who share their opinions on a more frequent basis usually become more comfortable 
+                    answering questions in class. Students who feel that their opinion holds equal weight to their 
+                    classmates will be more willing to speak up in class. A classroom environment that holds everyone's 
+                    opinions equally puts introverts more at ease and allows extroverts the ability to talk without 
+                    overpowering introverted classmates. 
                 </p>
             </div>
             <img src={sharedThings} alt="two people shaking hands" />
